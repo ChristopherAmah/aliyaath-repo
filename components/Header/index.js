4210ab06bd53106b96a430448147189865a15495
Christopher Amah <christopheramah0@gmail.com>
2025-12-02 12:13:34 +0100

    update

--- components/Header/index.js
+++ components/Header/index.js
@@ -76,9 +76,8 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   )}
                   {showResume && (
                     <Button
-                      onClick={() =>
-                        window.open("mailto:Oaliyyath@gmail.com")
-                      }
+                      onClick={() => router.push("/resume")}
+                      classes="first:ml-1"
                     >
                       Resume
                     </Button>
